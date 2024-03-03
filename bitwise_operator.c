b3203bce034d4b66c1f2a009b5945e1ce2172264
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-03-03 20:39:50 +0530

    adding program

---
+++ bitwise_operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a=25;
+	int b=16;
+	printf("AND value is: %d\n",a&b);
+	printf("OR value is: %d\n",a|b);
+	printf("XOR value is: %d\n",a^b);
+	printf("left shift value is: %d\n",a<<2);
+	printf("right shift value is: %d\n",a>>2);
+}

8d5da822f97b3de6bd60f05ddcd323e5910dd9cc
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-03-05 20:56:31 +0530

    adding program

---
+++ compare_two_string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+//for comparing	
+	char a[10];
+	printf("enter the string");
+	gets(a);
+	char b[10];
+	printf("enter the string");
+	gets(b);
+	int c=strcmp(a,b);
+	if(c==0){
+		printf("equal");
+		
+	}else{
+		printf("not equal");
+	}
+}

5fd1846d74c548c0e58cd57b22862435d3e4536d
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-03-08 22:24:40 +0530

    adding program

---
+++ for_loop.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	int i;
+	for(i=0;i<=2000000;i++){
+printf("%d",i);	
+}
+}
+
+
+/*
+
+#include<stdio.h>
+int main(){
+int i;
+for(i=0;i<=12;i++){
+	printf("\n hello himanshu",i);
+}
+}
+
+*/

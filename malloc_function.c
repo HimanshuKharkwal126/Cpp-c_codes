4f5fc268e58b1d5be86a2027d538edfaec9562bb
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-04-01 21:59:02 +0530

    adding program

---
+++ malloc_function.c
@@ -0,0 +1,22 @@
+                        // malloc function ....
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int i,n,sum=0,*ptr;
+	printf(" enter the total element:");
+	scanf("%d",&n);
+	ptr=(int*)malloc(n*sizeof(int));
+	if(ptr == NULL){
+		printf(" error! memory not allocated!");
+		exit(0);
+		
+	}
+		printf(" enter the element"\n);
+		for(i=0;i<n;i++){
+			scanf("%d",ptr + 1);
+			sum += *(ptr + 1);
+		}
+		printf(" sum = %d",sum);
+		free(ptr);
+	return 0;
+}

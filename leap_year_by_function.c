d7c3d6aade94f64152efb24f11fb7b8b0b6e3947
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-04-03 22:08:40 +0530

    adding program

---
+++ leap_year_by_function.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+	int leapYearFunction(int y);
+	int leapYearFunction(int y){
+		if(y%4==0 && y%100!=0 || y%400==0){
+			printf("leap year");
+		}else{
+			printf("not a leap year");
+		}
+		}
+		int main(){
+			int y;
+			printf("enter the year");
+			scanf("%d",&y);
+			leapYearFunction(y);
+	}

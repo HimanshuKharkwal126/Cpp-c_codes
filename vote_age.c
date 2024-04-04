babda9462cc07715659c9697e09c5de22e88482d
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-04-04 21:08:36 +0530

    adding program

---
+++ vote_age.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int x;
+	printf("enter the age x");
+	scanf("%d",&x);
+	if (x>18)
+	{
+	printf("you can vote");}
+	else{
+	printf("you can't vote,go and study");
+	}
+}

243e649f10de001055e0404d2b2c9c4e04f0723d
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-04-10 21:22:53 +0530

    adding program

---
+++ pattern_characters.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+int i,j,rows;
+	printf("enter the rows\n");
+	scanf("%d",&rows);
+	for(i=65;i<65+rows;i++){
+	for(j=65;j<=i;j++){
+		printf("%c",j);
+		
+	}                                 //  pattern of charecters 
+	printf("\n");
+	}
+}

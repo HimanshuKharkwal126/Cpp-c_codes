c565b9e2383af0b656f6cf9b5144ee416b88b2f1
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-04-10 21:25:54 +0530

    adding program

---
+++ right_half_trangular_pattern.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int i,j,rows;
+	printf("enter the rows");
+	scanf("%d",&rows);
+	for(i=0;i<=rows;i++){
+	for(j=0;j<=i;j++){
+		printf("*");
+	}
+	printf("\n");
+	}
+}

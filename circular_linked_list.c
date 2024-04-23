39d3b953f43b7b02b174f4453fe58640a8fba78d
HimanshuKharkwal126 <126himanshu@gmail.com>
2024-04-23 22:32:39 +0530

    adding program

---
+++ circular_linked_list.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+	int data;
+	struct node *next;
+};
+void traversing(struct node *first){
+	struct node *ptr=first;
+	do{
+			printf("element is %d\n",ptr->data);
+		ptr=ptr->next;
+	}
+	while(ptr!=first);
+}	
+int main(){
+struct node *first,*second,*third,*fourth,*fifth;
+first=(struct node *)malloc(sizeof(struct node));
+second=(struct node*)malloc(sizeof(struct node));
+third=(struct nde*)malloc(sizeof(struct node));
+fourth=(struct node*)malloc(sizeof(struct node));
+fifth=(struct node*)malloc(sizeof(struct node));
+first->data=5;
+first->next=second;
+second->data=10;
+second->next=third;
+third->data=15;
+third->next=fourth;
+fourth->data=20;
+fourth->next=fifth;
+fifth->data=25;
+fifth->next=first;
+traversing(first);
+}
